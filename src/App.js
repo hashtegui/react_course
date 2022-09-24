2ae7e8d84ada09119160bfe527973a5cc425c8db
Guilherme Castro de Souza <guilherme.castro.souza1999@gmail.com>
2022-09-24 13:47:24 -0400

    data fetch

--- src/App.js
+++ src/App.js
@@ -6,14 +6,25 @@ class App extends Component {
     posts: [],
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts");
+
+    const [posts] = await Promise.all([postsResponse]);
+
+    const postsJson = await posts.json();
+    this.setState({ posts: postsJson });
+  };
 
   render() {
     const { posts } = this.state;
     return (
-      <div className="App">
+      <div className="posts">
         {posts.map((post) => (
-          <div key={post.id}>
+          <div key={post.id} className="post-content">
             <h1>{post.title}</h1>
             <p>{post.body}</p>
           </div>
