910b8ffc7724aaf50a394872df551d0a1ecc511c
Guilherme Castro de Souza <guilherme.castro.souza1999@gmail.com>
2022-09-24 14:41:50 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import "./App.css";
-import { PostCard } from "./components/PostCard";
+import { Posts } from "./components/Posts";
 import { loadPosts } from "./utils/load-posts";
 
 class App extends Component {
@@ -15,19 +15,10 @@ class App extends Component {
 
   render() {
     const { posts } = this.state;
+
     return (
       <section className="container">
-        <div className="posts">
-          {posts.map((post) => (
-            <PostCard
-              key={post.id}
-              title={post.title}
-              cover={post.cover}
-              id={post.id}
-              body={post.body}
-            />
-          ))}
-        </div>
+        <Posts posts={posts} />
       </section>
     );
   }
--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -3,7 +3,7 @@ export const PostCard = ({ id, cover, title, body }) => {
     <div className="post">
       <img src={cover} alt={title} />
       <div className="post-content">
-        <h1>{title}</h1>
+        <h2>{title}</h2>
         <p>{body}</p>
       </div>
     </div>
