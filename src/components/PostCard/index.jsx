d5cb3e07660bffca462d85d7b9632c082cb2c005
Guilherme Castro de Souza <guilherme.castro.souza1999@gmail.com>
2022-09-24 14:26:19 -0400

    create component PostCard

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,11 @@
+export const PostCard = ({ id, cover, title, body }) => {
+  return (
+    <div className="post">
+      <img src={cover} alt={title} />
+      <div className="post-content">
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    </div>
+  );
+};
