f6bc459dfe985346986eebda9f72277cd8cb8a73
Guilherme Castro de Souza <guilherme.castro.souza1999@gmail.com>
2022-09-24 14:34:22 -0400

    refactoring loadPosts to another file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import { Component } from "react";
 import "./App.css";
 import { PostCard } from "./components/PostCard";
+import { loadPosts } from "./utils/load-posts";
 
 class App extends Component {
   state = {
     posts: [],
   };
 
-  componentDidMount() {
-    this.loadPosts();
-  }
-
-  loadPosts = async () => {
-    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts");
-    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos");
-
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
-
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url };
-    });
+  async componentDidMount() {
+    const postsAndPhotos = await loadPosts();
     this.setState({ posts: postsAndPhotos });
-  };
+  }
 
   render() {
     const { posts } = this.state;
